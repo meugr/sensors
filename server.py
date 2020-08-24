191cf375b4c674e8b1d4053be4cd655ff70938bf
meugr <meugr32@gmail.com>
2020-08-24 12:26:31 +0300

    cosmetic

--- server.py
+++ server.py
@@ -17,7 +17,6 @@ class LastHandler(tornado.web.RequestHandler):
     async def get(self):
         data = DataStorage.get_data(-1)
         old_data = DataStorage.get_data(end=-1)
-        print(len(old_data))
         await self.render("html/index.html", title="My title", data=data[0], old_data=old_data)
 
 
