1f33ae776bc545f76a7154c45f1eeb9ff19852c9
meugr <meugr32@gmail.com>
2020-11-09 22:44:57 +0300

    add schema.png

---
+++ t.py
@@ -0,0 +1,9 @@
+from tornado.httpclient import AsyncHTTPClient
+
+async def asynchronous_fetch(url):
+    http_client = AsyncHTTPClient()
+    response = await http_client.fetch(url)
+    return response.body
+
+
+asynchronous_fetch('http://10.8.0.15:5000')
